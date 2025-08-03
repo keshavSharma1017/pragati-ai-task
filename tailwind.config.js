04c91207304ec0bcb7610c9a653e7ef98111f7a3
keshav sharma <keshavbusharma2001@gmail.com>
2025-08-04 00:08:16 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      zIndex: {
+        '100': '1000', 
+      }
+    },
+  },
+  plugins: [],
+}
+
